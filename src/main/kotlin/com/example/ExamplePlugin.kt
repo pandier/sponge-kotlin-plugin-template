0e94499921fec8e40dbaea3a86c7fe73b1c24507
pandier <firegox@email.cz>
2025-05-09 10:39:36 +0200

    Use proper Logger class

--- src/main/kotlin/com/example/ExamplePlugin.kt
+++ src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,11 +1,11 @@
 package com.example
 
 import com.google.inject.Inject
+import org.apache.logging.log4j.Logger
 import org.spongepowered.api.Server
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.lifecycle.StartedEngineEvent
 import org.spongepowered.plugin.builtin.jvm.Plugin
-import java.util.logging.Logger
 
 @Plugin("example")
 class ExamplePlugin @Inject constructor(
